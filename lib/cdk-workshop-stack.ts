95cbaa7d655ffc95a6c3400ea48cee49d17bdfa6
hideyuki-matsuyama <choripon@gmail.com>
2022-09-14 17:10:52 +0900

    Up to 100-construct-testing.html

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,32 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super( scope, id, props );
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function( this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,            // execution environment
+      code: lambda.Code.fromAsset( 'lambda' ),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                        // file is "hello", function is "handler"
+    } );
+
+    const helloWithCounter = new HitCounter( this, 'HelloHitCounter', {
+      downstream: hello
+    } )
+
+    // defines an API Gateway REST API resource backend by out "hello" function.
+    new apigw.LambdaRestApi( this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    } );
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
